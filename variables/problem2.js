800094fa8c802b13aea4e96ce9638cba9c263d59
Aadrsh singh <aadrsh0207@gmail.com>
2022-10-12 18:08:12 +0530

    added variable

---
+++ variables/problem2.js
@@ -0,0 +1,6 @@
+let name="aadrsh singh";
+console.log("my name is",name);
+let fname="balbeer singh";
+console.log("my fathers name is",fname);
+let mname="kanya singh";
+console.log("my mothers name is",mname);
